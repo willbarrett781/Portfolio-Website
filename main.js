53e1cece10a2f64b1ff433d64d3a6af15528163c
willbarrett781 <willbarrett781@gmail.com>
2024-02-15 11:20:10 -0500

    Clean up code

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
-//window.alert("Jello");
+
 let projectCover = document.querySelector('.cover');
 let project1 = document.querySelector('.project-1');
-/*console.log(projectCover);
-console.log('test');
-console.log(document.querySelector('h1'))
 
-let projectHeader = 
-projectCover.style.backgroundColor = 'yellow';*/
 function addCover(event){
 projectCover.style.display = 'flex';
-console.log(event);
 }
+
 function removeCover(event){
     projectCover.style.display = 'none';
 }
 
-console.log(project1);
 project1.addEventListener('mouseover', addCover)
 project1.addEventListener('mouseout',removeCover)
